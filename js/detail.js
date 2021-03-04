9d3a744f9d749438ce80aec75881b14049c34248
hyejin <dzzd777@naver.com>
2021-03-04 18:34:17 +0900

    추가: 디테일 페이지 json 작업 추가
    상단에는 영화 포스터, 제목, 년도, 영화 소개, 좋아요, 평점 등이 있으며 하단에는 모든 장르의 영화 슬라이드가 있다

--- js/detail.js
+++ js/detail.js
@@ -1,29 +1,70 @@
 $(function () {
-    function getData(page) {
-    let getDatas =[];
+  // detail ajax
+  let detailID = document.location.href.split("=")[1];
+      $.ajax({
+          url: "https://yts.mx/api/v2/movie_details.json?movie_id=" + detailID,
+          success: function (detailData) {
+            console.log(detailData);
+            let detailTitle = `${detailData.data.movie.title}`;
+            $(".header_title").append(detailTitle);
+            let detailMovie = `
+                          <div class="movieImg">
+                              <img src="${detailData.data.movie.medium_cover_image}" alt="" />
+                            </div>
+                            <div class="movieTxt">
+                              <h2 class="movieTitle">${detailData.data.movie.title_long}</h2>
+                              <h3 class="movieGenre"><span>장르:</span> ${detailData.data.movie.genres}</h3>
+                              <h3><span>시놉시스:</span> </h3>
+                              <p class="movieDes">${detailData.data.movie.description_full}</p>
+                              <h3 class="movieRate"><span>평점:</span> ${detailData.data.movie.rating}</h3>
+                              <h3><span>좋아요:</span> ${detailData.data.movie.like_count}</h3>
+                            </div>
+                        `;
+                        $(".mainTitle").append(detailMovie);
+          }
+      });
+  
+  // all genre ajax
+    function getgenre(gen, box, slideID) {
+    let getGenres = [];
     $.ajax({
-      url: 'https://yts.mx/api/v2/movie_details.json?movie_id=',
-      success: function (data) {
-        console.log(data);
-        // for(let i = 0; i < data.data.movies.length; i++){
-        //   if(data.data.movies[i].title == ''){
-        //     data.data.movies[i].title = 'No Title';
-        //   }
-        //   let recentHTML = `<div class="recent-movie-wrap">
-        //                       <a href="https://yts.mx/api/v2/movie_details.json?movie_id=${data.data.movies[i].id}"  target="_blank">                         
-        //                         <div class="recent-movies">
-        //                           <div class="movie-img">
-        //                           <img src="${data.data.movies[i].medium_cover_image}" alt="" />
-        //                           </div>
-        //                           <h3 class="movie-title">${data.data.movies[i].title}</h3>
-        //                           </div>
-        //                       </a> 
-        //                     </div>`;
-        //   getDatas += recentHTML;
-        // }
-        // $(".container").append(getDatas);
-      }
+    url: 'https://yts.mx/api/v2/list_movies.json?genre='+gen+'&page=1&limit=15',
+    success: function (data) {
+      // console.log(data);
+      for (let i = 0; i < data.data.movies.length; i++){
+        let genreHTML = `
+                                  <div class="slide-box">
+                                  <a href="/movie_api/genre/detail.html?id=${data.data.movies[i].id}">
+                                    <div>
+                                      <img src="${data.data.movies[i].medium_cover_image}" alt="" />
+                                      <div class="slider-tit">
+                                        <h3 class="movie_title">${data.data.movies[i].title}</h3>
+                                        <p class="rating">Rating : 8.0${data.data.movies[i].rating}</p>
+                                      </div>
+                                    </div>
+                                    </a>
+                                  </div>
+        
+                        `;
+                        getGenres += genreHTML;
+          }
+          $(box).append(getGenres);
+        
+        // slide
+          $(slideID).lightSlider({
+            item: 5,
+            slideMove: 1,
+            auto: true,
+            loop: true,
+            speed: 400,
+          });  
+        }
     });
-    currentPage = page;
   }
-});
+  getgenre('all', '.all-contents', '#allSlide');
+  
+  // loading
+      $(document).ajaxComplete(function () {
+      $(".loading").hide();
+    });
+  });
