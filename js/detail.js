adcde396d224c66273bb25a6e009e11d241bbe3e
hyejin <dzzd777@naver.com>
2021-03-05 14:24:32 +0900

    수정: 대체 이미지 추가, lightslider 반응형 추가
    onError 속성으로 404 error가 뜰 시 대체 이미지 추가
    lightslider 반응형에 따른 객체 추가

--- js/detail.js
+++ js/detail.js
@@ -4,16 +4,15 @@ $(function () {
       $.ajax({
           url: "https://yts.mx/api/v2/movie_details.json?movie_id=" + detailID,
           success: function (detailData) {
-            console.log(detailData);
             let detailTitle = `${detailData.data.movie.title}`;
             $(".header_title").append(detailTitle);
             let detailMovie = `
                           <div class="movieImg">
-                              <img src="${detailData.data.movie.medium_cover_image}" alt="" />
+                              <img src="${detailData.data.movie.medium_cover_image}" alt="" onError="this.src='/movie_api/img/replace.jpg';" />
                             </div>
                             <div class="movieTxt">
                               <h2 class="movieTitle">${detailData.data.movie.title_long}</h2>
-                              <h3 class="movieGenre"><span>장르:</span> ${detailData.data.movie.genres}</h3>
+                              <h3 class="movieGenre"><span>장르:</span> ${detailData.data.movie.genres.join(', ')}</h3>
                               <h3><span>시놉시스:</span> </h3>
                               <p class="movieDes">${detailData.data.movie.description_full}</p>
                               <h3 class="movieRate"><span>평점:</span> ${detailData.data.movie.rating}</h3>
@@ -36,7 +35,7 @@ $(function () {
                                   <div class="slide-box">
                                   <a href="/movie_api/genre/detail.html?id=${data.data.movies[i].id}">
                                     <div>
-                                      <img src="${data.data.movies[i].medium_cover_image}" alt="" />
+                                      <img src="${data.data.movies[i].medium_cover_image}" alt="" onError="this.src='/movie_api/img/replace.jpg';"/>
                                       <div class="slider-tit">
                                         <h3 class="movie_title">${data.data.movies[i].title}</h3>
                                         <p class="rating">Rating : 8.0${data.data.movies[i].rating}</p>
@@ -50,14 +49,31 @@ $(function () {
           }
           $(box).append(getGenres);
         
-        // slide
+                // lightslide
           $(slideID).lightSlider({
             item: 5,
             slideMove: 1,
             auto: true,
             loop: true,
             speed: 400,
-          });  
+            responsive : [
+            {
+                    breakpoint:800,
+                    settings: {
+                        item:4,
+                        slideMove:1,
+                        slideMargin:6,
+                      }
+                },
+                {
+                    breakpoint:480,
+                    settings: {
+                        item:2,
+                        slideMove:1
+                      }
+                }
+              ]
+          });
         }
     });
   }
--- js/general.js
+++ js/general.js
@@ -10,7 +10,7 @@ $(function () {
                                   <div class="slide-box">
                                     <a href="/movie_api/genre/detail.html?id=${data.data.movies[i].id}">
                                       <div>
-                                        <img src="${data.data.movies[i].medium_cover_image}" alt="" />
+                                        <img src="${data.data.movies[i].medium_cover_image}" alt="" onError="this.src='/movie_api/img/replace.jpg';"/>
                                         <div class="slider-tit">
                                           <h3 class="movie_title">${data.data.movies[i].title}</h3>
                                           <p class="rating">Rating : 8.0${data.data.movies[i].rating}</p>
@@ -24,14 +24,33 @@ $(function () {
         }
         $(box).append(getGenres);
         
-        // slide
-        $(slideID).lightSlider({
-          item: 5,
-          slideMove: 1,
-          auto: true,
-          loop: true,
-          speed: 400,
-        });
+        // lightslide
+          $(slideID).lightSlider({
+            item: 5,
+            slideMove: 1,
+            auto: true,
+            loop: true,
+            speed: 400,
+            responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1
+                  }
+            }
+        ]
+          });
+      
+        // success function end
       }
     });
   }
@@ -42,6 +61,7 @@ $(function () {
     getgenre(getGenres[i], '.' + getGenres[i] + '-contents', '#'+getGenres[i] + 'Slide');
   }
 
+  // click event
   $(".category ul li").click(function (e) {
     $(".category ul li").removeClass("action");
     $(this).toggleClass("action");
@@ -55,6 +75,7 @@ $(function () {
     }
   });
 
+  // loading...
     $(document).ajaxComplete(function () {
     $(".loading").hide();
   });
